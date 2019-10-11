833692b934448f2a5854b0b357535df4f9a6f4f3
lee minju <dptmfkdls120@gmail.com>
2019-10-11 23:11:49 +0900

    finish chaincode
    finish chaincode install, instantiate, invoke

--- Hyperledger-Fabric-Tuna-App/basic-network/cc_start_example02.sh
+++ Hyperledger-Fabric-Tuna-App/basic-network/cc_start_example02.sh
@@ -7,82 +7,82 @@ CC_SRC_PATH=github.com/chaincode_example02/go
 CC_NAME=example02
 CC_VERSION=1.0
 
-docker-compose -f ./docker-compose.yml up -d cli_org1 cli_org2
+docker-compose -f ./docker-compose.yml up -d cli_GovernmentOrg cli_SupportingEnterpriseOrg
 docker ps -a
-# Org1
+# GovernmentOrg
 echo ==========================================================================
-echo        Org1
+echo        GovernmentOrg
 echo ==========================================================================
-echo  install chaincode to peer0.org1.example.com
-#install chaincode to peer0.org1.example.com - 각 endoser peer에 모두 설치 
-docker exec  cli_org1 peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+echo  install chaincode to peer0.GovernmentOrg.example.com
+#install chaincode to peer0.GovernmentOrg.example.com - 각 endoser peer에 모두 설치 
+docker exec  cli_GovernmentOrg peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 echo  instantiate chaincode to mychannel
 #instantiate chaincode - 채널 당 한번만 실행 
 # 인스턴스 생성 docker ps -a 해보면
-# dev-peer0.org1.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
-docker exec  cli_org1 peer chaincode instantiate -o orderer.example.com:7050 -C "$CHANNEL_NAME" -n "$CC_NAME" -l "$CC_RUNTIME_LANGUAGE" -v "$CC_VERSION" -c '{"Args":["init","a","100","b","200"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+# dev-peer0.GovernmentOrg.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
+docker exec  cli_GovernmentOrg peer chaincode instantiate -o orderer.example.com:7050 -C "$CHANNEL_NAME" -n "$CC_NAME" -l "$CC_RUNTIME_LANGUAGE" -v "$CC_VERSION" -c '{"Args":["init","a","100","b","200"]}' -P "OR ('GovernmentOrgMSP.member','SupportingEnterpriseOrgMSP.member')"
 sleep 5
 
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
-docker exec  peer0.org1.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","10"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","10"]}'
 sleep 5
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 echo ==========================================================================
-echo  install chaincode to peer1.org1.example.com
-#install chaincode to peer1.org1.example.com - 각 endoser peer에 모두 설치 
-docker exec -e CORE_PEER_ADDRESS=peer1.org1.example.com:18051  cli_org1 peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+echo  install chaincode to peer1.GovernmentOrg.example.com
+#install chaincode to peer1.GovernmentOrg.example.com - 각 endoser peer에 모두 설치 
+docker exec -e CORE_PEER_ADDRESS=peer1.GovernmentOrg.example.com:18051  cli_GovernmentOrg peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 # endoser peer에서 처음 query 수행하면 인스턴스 생성됨 docker ps -a 해보면
-# dev-peer1.org1.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
-docker exec  peer1.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+# dev-peer1.GovernmentOrg.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
+docker exec  peer1.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
 sleep 5
-docker exec  peer1.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
-docker exec  peer1.org1.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
+docker exec  peer1.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer1.GovernmentOrg.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
 sleep 5
-docker exec  peer1.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer1.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer1.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer1.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 
-# Org2
+# SupportingEnterpriseOrg
 echo ==========================================================================
-echo        Org2
+echo        SupportingEnterpriseOrg
 echo ==========================================================================
-echo  install chaincode to peer0.org2.example.com
-#install chaincode to peer0.org2.example.com - 각 endoser peer에 모두 설치 
-docker exec -e CORE_PEER_ADDRESS=peer0.org2.example.com:27051  cli_org2   peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+echo  install chaincode to peer0.SupportingEnterpriseOrg.example.com
+#install chaincode to peer0.SupportingEnterpriseOrg.example.com - 각 endoser peer에 모두 설치 
+docker exec -e CORE_PEER_ADDRESS=peer0.SupportingEnterpriseOrg.example.com:27051  cli_SupportingEnterpriseOrg   peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 # endoser peer에서 처음 query 수행하면 인스턴스 생성됨 docker ps -a 해보면
-# dev-peer0.org2.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
-# docker exec  -e CORE_PEER_ADDRESS=peer0.org2.example.com:7051  cli_org2   peer chaincode query -o orderer.example.com:7050 -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+# dev-peer0.SupportingEnterpriseOrg.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
+# docker exec  -e CORE_PEER_ADDRESS=peer0.SupportingEnterpriseOrg.example.com:7051  cli_SupportingEnterpriseOrg   peer chaincode query -o orderer.example.com:7050 -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
 sleep 5
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
-# docker exec  -e CORE_PEER_ADDRESS=peer0.org2.example.com:7051   cli_org2   peer chaincode invoke -o orderer.example.com:7050  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
-docker exec  peer0.org2.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+# docker exec  -e CORE_PEER_ADDRESS=peer0.SupportingEnterpriseOrg.example.com:7051   cli_SupportingEnterpriseOrg   peer chaincode invoke -o orderer.example.com:7050  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
 sleep 5
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 echo ==========================================================================
-echo  install chaincode to peer1.org2.example.com
-#install chaincode to peer1.org2.example.com - 각 endoser peer에 모두 설치 
-docker exec -e CORE_PEER_ADDRESS=peer1.org2.example.com:28051  cli_org2 peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+echo  install chaincode to peer1.SupportingEnterpriseOrg.example.com
+#install chaincode to peer1.SupportingEnterpriseOrg.example.com - 각 endoser peer에 모두 설치 
+docker exec -e CORE_PEER_ADDRESS=peer1.SupportingEnterpriseOrg.example.com:28051  cli_SupportingEnterpriseOrg peer chaincode install -n "$CC_NAME" -v "$CC_VERSION" -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
 # endoser peer에서 처음 query 수행하면 인스턴스 생성됨 docker ps -a 해보면
-# dev-peer1.org2.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
-docker exec  peer1.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+# dev-peer1.SupportingEnterpriseOrg.example.com-example02-1.0-xxxx 식의 컨테이너 생성됨
+docker exec  peer1.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
 sleep 5
-docker exec  peer1.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
-docker exec  peer1.org2.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
+docker exec  peer1.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer1.SupportingEnterpriseOrg.example.com peer chaincode invoke  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["invoke","a","b","5"]}'
 sleep 5
-docker exec  peer1.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer1.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer1.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer1.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org2.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.SupportingEnterpriseOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
-docker exec  peer0.org1.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","a"]}'
+docker exec  peer0.GovernmentOrg.example.com peer chaincode query  -C "$CHANNEL_NAME" -n "$CC_NAME" -c '{"Args":["query","b"]}'
 
 cat <<EOF
 Total setup execution time : $(($(date +%s) - starttime)) secs ...
--- Hyperledger-Fabric-Tuna-App/tuna-app/startFabric.sh
+++ Hyperledger-Fabric-Tuna-App/tuna-app/startFabric.sh
@@ -18,17 +18,30 @@ if [ ! -d ~/.hfc-key-store/ ]; then
 fi
 
 # launch network; create channel and join peer to channel
-cd ../basic-network
+cd ../basic-network/
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 tuna catches
-docker-compose -f ./docker-compose.yml up -d cli
+docker-compose -f ./docker-compose.yml up -d cli_GovernmentOrg  cli_SupportingEnterpriseOrg  cli_RecipientOrg  cli_SupporterOrg
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
-sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 4개조직 모두 참여 채널
+echo 정부
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+#채널당 한번만 instantiate
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode instantiate -o orderer.example.com:7050 -C mychannel1 -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('GovernmentOrgMSP.member','SupportingEnterpriseMSP.member', 'RecipientMSP.member', 'SupporterMSP.member')"
+sleep 10 
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 후원업체
+docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 후원자
+docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 피후원자
+docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
 printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n"
