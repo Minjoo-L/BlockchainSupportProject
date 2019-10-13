a7535294d81265b883a6cd3e8c65619376363849
Soyoung <yooso020599@naver.com>
2019-10-13 16:32:38 +0900

    channeladd

--- Hyperledger-Fabric-Tuna-App/tuna-app/startFabric.sh
+++ Hyperledger-Fabric-Tuna-App/tuna-app/startFabric.sh
@@ -35,13 +35,46 @@ docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPA
 echo 후원업체
 docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
 docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
-echo 후원자
+echo 피후원자
 docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
 docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
-echo 피후원자
+echo 후원자
 docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
 docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel1 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
 
+echo channel2
+echo 정부
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+#채널당 한번만 instantiate
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode instantiate -o orderer.example.com:7050 -C mychannel2 -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('GovernmentOrgMSP.member','SupportingEnterpriseMSP.member', 'RecipientMSP.member', 'SupporterMSP.member')"
+sleep 10 
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel2 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+
+echo 후원자
+docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=SupporterOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupporterOrg.example.com/users/Admin@SupporterOrg.example.com/msp" cli_SupporterOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel2 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+
+echo channel3
+echo 정부
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+#채널당 한번만 instantiate
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode instantiate -o orderer.example.com:7050 -C mychannel3 -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('GovernmentOrgMSP.member','SupportingEnterpriseMSP.member', 'RecipientMSP.member', 'SupporterMSP.member')"
+sleep 10 
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel3 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 피후원자
+docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=RecipientOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/RecipientOrg.example.com/users/Admin@RecipientOrg.example.com/msp" cli_RecipientOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel3 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+
+echo channel4
+echo 정부
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+#채널당 한번만 instantiate
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode instantiate -o orderer.example.com:7050 -C mychannel4 -n tuna-app -v 1.0 -c '{"Args":[""]}' -P "OR ('GovernmentOrgMSP.member','SupportingEnterpriseMSP.member', 'RecipientMSP.member', 'SupporterMSP.member')"
+sleep 10 
+docker exec -e "CORE_PEER_LOCALMSPID=GovernmentOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/GovernmentOrg.example.com/users/Admin@GovernmentOrg.example.com/msp" cli_GovernmentOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel4 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
+echo 후원업체
+docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode install -n tuna-app -v 1.0 -p github.com/tuna-app
+docker exec -e "CORE_PEER_LOCALMSPID=SupportingEnterpriseOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/SupportingEnterpriseOrg.example.com/users/Admin@SupportingEnterpriseOrg.example.com/msp" cli_SupportingEnterpriseOrg peer chaincode invoke -o orderer.example.com:7050 -C mychannel4 -n tuna-app -c '{"function":"initLedger","Args":[""]}'
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
 printf "\nStart with the registerAdmin.js, then registerUser.js, then server.js\n\n"
