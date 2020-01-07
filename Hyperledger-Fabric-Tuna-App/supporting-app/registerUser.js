75d00c4b42925804073864f5ad218bb9927ba0a0
Soyoung Yoo <yooso020599@naver.com>
2020-01-08 03:49:58 +0900

    add Org Admins and Users

--- Hyperledger-Fabric-Tuna-App/supporting-app/registerUser.js
+++ Hyperledger-Fabric-Tuna-App/supporting-app/registerUser.js
@@ -22,21 +22,25 @@ var fabric_client = new Fabric_Client();
 var fabric_ca_client = null;
 var admin_user = null;
 var member_user = null;
+var crypto_suite =null;
+var crypto_store = null;
+var tlsOptions = null;
 var store_path = path.join(os.homedir(), '.hfc-key-store');
 console.log(' Store path:'+store_path);
 
+//GovernmentOrg 유저 생성
 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 Fabric_Client.newDefaultKeyValueStore({ path: store_path
 }).then((state_store) => {
     // assign the store to the fabric client
     fabric_client.setStateStore(state_store);
-    var crypto_suite = Fabric_Client.newCryptoSuite();
+    crypto_suite = Fabric_Client.newCryptoSuite();
     // use the same location for the state store (where the users' certificate are kept)
     // and the crypto store (where the users' keys are kept)
-    var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+    crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
     crypto_suite.setCryptoKeyStore(crypto_store);
     fabric_client.setCryptoSuite(crypto_suite);
-    var	tlsOptions = {
+    tlsOptions = {
     	trustedRoots: [],
     	verify: false
     };
@@ -55,7 +59,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
     // at this point we should have the admin user
     // first need to register the user with the CA server
-    return fabric_ca_client.register({enrollmentID: 'user1', affiliation: 'org1.department1'}, admin_user);
+    return fabric_ca_client.register({enrollmentID: 'user1', affiliation: ''}, admin_user);
 }).then((secret) => {
     // next we need to enroll the user with CA server
     console.log('Successfully registered user1 - secret:'+ secret);
@@ -65,7 +69,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
   console.log('Successfully enrolled member user "user1" ');
   return fabric_client.createUser(
      {username: 'user1',
-     mspid: 'Org1MSP',
+     mspid: 'GovernmentOrgMSP',
      cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
      });
 }).then((user) => {
@@ -81,4 +85,127 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 		console.error('Authorization failures may be caused by having admin credentials from a previous CA instance.\n' +
 		'Try again after deleting the contents of the store directory '+store_path);
 	}
-});
+}).then(()=>{ //SupportingEnterprise User 등록
+    fabric_ca_client = new Fabric_CA_Client('http://localhost:27054', null , '', crypto_suite);
+
+    // first check to see if the admin is already enrolled
+    return fabric_client.getUserContext('admin2', true);
+}).then((user_from_store) => {
+    if (user_from_store && user_from_store.isEnrolled()) {
+        console.log('Successfully loaded admin2 from persistence');
+        admin_user = user_from_store;
+    } else {
+        throw new Error('Failed to get admin2.... run registerAdmin.js');
+    }
+
+    // at this point we should have the admin user
+    // first need to register the user with the CA server
+    return fabric_ca_client.register({enrollmentID: 'user2', affiliation: ''}, admin_user);
+}).then((secret) => {
+    // next we need to enroll the user with CA server
+    console.log('Successfully registered user2 - secret:'+ secret);
+
+    return fabric_ca_client.enroll({enrollmentID: 'user2', enrollmentSecret: secret});
+}).then((enrollment) => {
+  console.log('Successfully enrolled member user "user2" ');
+  return fabric_client.createUser(
+     {username: 'user2',
+     mspid: 'SupportingEnterpriseOrgMSP',
+     cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
+     });
+}).then((user) => {
+     member_user = user;
+
+     return fabric_client.setUserContext(member_user);
+}).then(()=>{
+     console.log('User2 was successfully registered and enrolled and is ready to intreact with the fabric network');
+
+}).catch((err) => {
+    console.error('Failed to register: ' + err);
+	if(err.toString().indexOf('Authorization') > -1) {
+		console.error('Authorization failures may be caused by having admin credentials from a previous CA instance.\n' +
+		'Try again after deleting the contents of the store directory '+store_path);
+	}
+}).then(()=>{ //Recipient User 등록
+    fabric_ca_client = new Fabric_CA_Client('http://localhost:37054', null , '', crypto_suite);
+
+    // first check to see if the admin is already enrolled
+    return fabric_client.getUserContext('admin3', true);
+}).then((user_from_store) => {
+    if (user_from_store && user_from_store.isEnrolled()) {
+        console.log('Successfully loaded admin3 from persistence');
+        admin_user = user_from_store;
+    } else {
+        throw new Error('Failed to get admin3.... run registerAdmin.js');
+    }
+
+    // at this point we should have the admin user
+    // first need to register the user with the CA server
+    return fabric_ca_client.register({enrollmentID: 'user3', affiliation: ''}, admin_user);
+}).then((secret) => {
+    // next we need to enroll the user with CA server
+    console.log('Successfully registered user3 - secret:'+ secret);
+
+    return fabric_ca_client.enroll({enrollmentID: 'user3', enrollmentSecret: secret});
+}).then((enrollment) => {
+  console.log('Successfully enrolled member user "user3" ');
+  return fabric_client.createUser(
+     {username: 'user3',
+     mspid: 'RecipientOrgMSP',
+     cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
+     });
+}).then((user) => {
+     member_user = user;
+
+     return fabric_client.setUserContext(member_user);
+}).then(()=>{
+     console.log('User3 was successfully registered and enrolled and is ready to intreact with the fabric network');
+
+}).catch((err) => {
+    console.error('Failed to register: ' + err);
+	if(err.toString().indexOf('Authorization') > -1) {
+		console.error('Authorization failures may be caused by having admin credentials from a previous CA instance.\n' +
+		'Try again after deleting the contents of the store directory '+store_path);
+	}
+}).then(()=>{ //Supporter User 등록
+    fabric_ca_client = new Fabric_CA_Client('http://localhost:47054', null , '', crypto_suite);
+
+    // first check to see if the admin is already enrolled
+    return fabric_client.getUserContext('admin4', true);
+}).then((user_from_store) => {
+    if (user_from_store && user_from_store.isEnrolled()) {
+        console.log('Successfully loaded admin4 from persistence');
+        admin_user = user_from_store;
+    } else {
+        throw new Error('Failed to get admin4.... run registerAdmin.js');
+    }
+
+    // at this point we should have the admin user
+    // first need to register the user with the CA server
+    return fabric_ca_client.register({enrollmentID: 'user4', affiliation: ''}, admin_user);
+}).then((secret) => {
+    // next we need to enroll the user with CA server
+    console.log('Successfully registered user4 - secret:'+ secret);
+
+    return fabric_ca_client.enroll({enrollmentID: 'user4', enrollmentSecret: secret});
+}).then((enrollment) => {
+  console.log('Successfully enrolled member user "user4" ');
+  return fabric_client.createUser(
+     {username: 'user4',
+     mspid: 'SupporterOrgMSP',
+     cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
+     });
+}).then((user) => {
+     member_user = user;
+
+     return fabric_client.setUserContext(member_user);
+}).then(()=>{
+     console.log('User4 was successfully registered and enrolled and is ready to intreact with the fabric network');
+
+}).catch((err) => {
+    console.error('Failed to register: ' + err);
+	if(err.toString().indexOf('Authorization') > -1) {
+		console.error('Authorization failures may be caused by having admin credentials from a previous CA instance.\n' +
+		'Try again after deleting the contents of the store directory '+store_path);
+	}
+})
