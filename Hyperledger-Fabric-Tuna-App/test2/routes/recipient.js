70a78e434e8e42b270b3cce3dc50e9dff82af42e
lee minju <dptmfkdls120@gmail.com>
2020-03-03 11:17:01 +0900

    로그인 페이지 다시 연결

--- Hyperledger-Fabric-Tuna-App/test2/routes/recipient.js
+++ Hyperledger-Fabric-Tuna-App/test2/routes/recipient.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();  
-var session = require('express-session');//로그인 세션 유지
+var session = require('express-session');
 
 router.get('/reci_query_result', function(req, res){
     res.render('reci_query_result',{
