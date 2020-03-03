2eab6373a1b6f0a8b5d1c02d03cb580b2dd2870b
lee minju <dptmfkdls120@gmail.com>
2020-03-03 16:14:30 +0900

    문제: 데이터 넘기기

--- Hyperledger-Fabric-Tuna-App/test2/routes/recipient.js
+++ Hyperledger-Fabric-Tuna-App/test2/routes/recipient.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();  
 var session = require('express-session');
+var channel1Query = require('../channel1.js');
 
-router.get('/reci_query_result', function(req, res){
+//const { Recipient } = require('../models'); 
+
+
+router.get('/reci_query_result', async function(req, res){
+    var recipients  = await channel1Query.query('queryAllRecipient');
+    var data = [];
+
+    console.log('좀 데이터 좀 보여줘.. 데이터 조회 되기 전에 니가 먼저 실행되는거니', `${recipients}`);
     res.render('reci_query_result',{
         session: session
     });
+
 });
 module.exports = router;
