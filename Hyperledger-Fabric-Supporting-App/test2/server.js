2d5146f7ffd68c28339c91f4d53f173233f8960b
Ubuntu <ubuntu@ip-172-31-47-110.ec2.internal>
2020-05-06 15:18:09 +0000

    ?

--- Hyperledger-Fabric-Supporting-App/test2/server.js
+++ Hyperledger-Fabric-Supporting-App/test2/server.js
@@ -149,7 +149,7 @@ app.post('/mypage', function(req,res){
         else if(rows.length==1){//로그인 성공
             sess.Name = rows[0].Name;
             sess.email = rows[0].Email;
-            sess.auth = rows[0].Auth;   // auth로 다시 바꿔주기
+            sess.auth = rows[0].auth;   // auth로 다시 바꿔주기
             console.log('success');
             console.log(sess.Name);
             res.render("mypage",{
